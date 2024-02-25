62c808733a31b92aa7e46455f4d1b598396a7d14
Swoyesh Shrestha <swoyeshshrestha69@gmail.com>
2024-02-25 21:20:58 +0545

    movie collection

--- src/components/Cmovie.js
+++ src/components/Cmovie.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import "../cMovie.css"
 
-const Cmovie = () => {
+const Cmovie = (props) => {
+    const time = ["09:00 AM", "09:30 AM", "11:00 AM", "03:00 PM"]
     return (
         <>
-            <div class="card" style={{width: "18rem", margin: "15px"}}>
-                <img src="..." class="card-img-top" alt="..." />
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="card" style={{width: "18rem", margin: "15px", border: "none"}}>
+                <img src={props.img} class="card-img-top" alt="..." />
+                <div class="card-body" style={{backgroundColor: "#002f5e"}}>
+                    <h5 class="card-title" style={{color: "white", fontSize: "21px"}}><strong>{props.title}</strong></h5>
+                    <p class="card-text" style={{color: "gray", fontSize: "12px", textAlign: "left"}}>{props.duration}</p>
+                    <p class="card-text" style={{color: "gray", marginTop: "-12px", fontSize: "12px", textAlign: "left"}}>{props.genre}</p>
+                    {time.map((element)=>{
+                        return <a href="#" class="btn btn-primary">{element}</a>
+                    })
+                    }
                 </div>
             </div>
         </>
--- src/components/Moviecollec.js
+++ src/components/Moviecollec.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Cmovie from './Cmovie'
+import p1 from "../p1.jpg"
+import m1 from "../m1.jpg"
 
 const Moviecollec = () => {
   return (
     <div className='container'>
-        <Cmovie/>
-        <Cmovie/>
+        <Cmovie title = {"Crakk - Jeetega Toh Jiyegaa"} duration = {"2 Hours 30 Mins"} genre = {"ACTION, SURVIVAL THRILLER"} img = {p1}/>
+        <Cmovie title = {"Teri Baaton Mein Aisa Uljha Jiya"} duration = {"2 Hours"} genre = {"DRAMA, ROMANCE"} img = {m1}/>
         <Cmovie/>
         <Cmovie/>
     </div>
