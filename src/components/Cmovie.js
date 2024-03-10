074233b2bc8ea42aab089aabb2a7902455a6e81b
Swoyesh Shrestha <swoyeshshrestha69@gmail.com>
2024-03-10 20:23:22 +0545

    final changes

--- src/components/Cmovie.js
+++ src/components/Cmovie.js
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../cMovie.css"
 
 const Cmovie = (props) => {
+    const [display1, setDisplay1] = useState(-140)
+    const[zoom, setZoom] = useState("1")
     const {type} = props
     const time = ["09:00 AM", "09:30 AM", "11:00 AM", "03:00 PM"]
+
+    const clicked1 = ()=>{
+        setZoom("1.1")
+        setDisplay1(0)
+    }
+
+    const clicked2 = ()=>{
+        setZoom("1")
+        setDisplay1(-140)
+    }
+
+    let s1 = {
+        translate: display1
+      }
+      let s2 = {
+        scale: zoom
+      }
     return (
         <>
             <div className="card" style={{width: "18rem", margin: "15px", border: "none"}}>
-            <div className='type' style={{position: "relative", top: "10px", backgroundColor: "red", height: "0%", textAlign: "right", right:"10px"}}>
-            <button className='b1'><i class="fa-regular fa-ticket" style={{margin: "8px"}}></i>Buy Tickets</button>
-                    <button className='b2'><i class="fa-thin fa-play" style={{margin: "8px"}}></i>Play Trailer</button>
+            <div style={s2} onMouseMove={clicked1} onMouseOut={clicked2} className='type' style={{position: "relative", top: "10px", backgroundColor: "red", height: "0%", textAlign: "right", right:"10px"}}>
+            <button className='b1' style={s1}><i class="fa-regular fa-ticket" style={{margin: "8px"}}></i>Buy Tickets</button>
+                    <button className='b2' style={s1}><i class="fa-thin fa-play" style={{margin: "8px"}}></i>Play Trailer</button>
                     <button className='buttt'>{type}</button>
                     
                 </div>
-                <div className='img-div'>
+                <div className='img-div' style={s2} onMouseMove={clicked1} onMouseOut={clicked2}>
                 <img src={props.img} className="card-img-top" alt="..." height={"380px"} />
+                <div className='mid'></div>
                 </div>
                 <div className="card-body" style={{backgroundColor: "#002f5e"}}>
                     <h5 className="card-title" style={{color: "white", fontSize: "21px"}}><strong>{props.title}</strong></h5>
