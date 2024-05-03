c98af0c862a1b43fa3dcff31bae641f31b003b24
Swoyesh Shrestha <swoyeshshrestha69@gmail.com>
2024-05-03 20:29:43 +0545

    frontend captcha left

--- src/components/Forgo.js
+++ src/components/Forgo.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import Footer from './Footer'
+import '../Forgo.css'
 
 const Forgo = () => {
   return (
     <>
-    <div className='container' style={{height: "100px", backgroundColor: "#081243"}}>
-        <div className='n-cont'>
-            <span style={{color: "#0e68a0", alignItems: "center"}}>Forgot Password</span>
+    <div className='container' style={{width: "35%", backgroundColor: "#081243", display: "flex", justifyContent: "center", boxShadow: "0px 0px 30px #fff", borderRadius: "6px", position: "relative", top: "60px", overflow:"hidden", border: "1px solid white", alignItems: "stretch"}}>
+        <div className='n-cont' style={{marginTop: "10px", marginBottom: "40px", width: "100%"}}>
+            <span style={{color: "#0e68a0", marginBottom: "15px", fontWeight: "bold", fontSize: "25px", display: "block", display: "flex", justifyContent: "center"}}>Forgot Password</span>
+            <div className='former'>
+              <form>
+                <input type='text' style={{position: "relative", borderRadius: "5px", border: "none", padding: "7px", width: "90%", backgroundColor: "#fff", marginLeft: "18px", justifyContent:"flex-start"}} className='iinp'></input>
+                <input type='text' style={{position: "relative", borderRadius: "5px", border: "none", padding: "7px", width: "90%", backgroundColor: "#fff", top: "20px", marginLeft: "18px"}} className='iinp' placeholder="Enter Captcha Text"></input>
+                <div style={{width: "80%"}}>
+                <button type='submit' className='sbtn'>Submit</button>
+                <button type='submit' className='cbtn'>Cancel</button>
+                </div>
+              </form>
+            </div>
         </div>
     </div>
-    <div className='bottom' style={{top: "60px"}}>
+    <div className='bottom' style={{top: "120px"}}>
       <Footer/>
     </div>
     </>
