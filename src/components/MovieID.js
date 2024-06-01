f5c52e8a861e8481f1876dba693b613bf90b5308
Swoyesh Shrestha <swoyeshshrestha69@gmail.com>
2024-06-01 19:07:39 +0545

    frontend progress

--- src/components/MovieID.js
+++ src/components/MovieID.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../MovieID.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../MovieID.css";
 
 const MovieID = () => {
   const sign1 = ">";
@@ -8,7 +8,7 @@ const MovieID = () => {
     <>
       <div style={{ display: "flex", flexDirection: "column" }}>
         <div
-          className='contss'
+          className="contss"
           style={{
             position: "fixed",
             margin: "15px 0px 0px 20px",
@@ -20,7 +20,7 @@ const MovieID = () => {
           <div>
             <h3
               style={{
-                float: 'left',
+                float: "left",
                 fontSize: "24px",
                 width: "100%",
                 fontWeight: "600",
@@ -31,7 +31,7 @@ const MovieID = () => {
             </h3>
             <span
               style={{
-                float: 'left',
+                float: "left",
                 color: "#e9ecef",
                 opacity: "0.75",
                 fontSize: "13px",
@@ -42,7 +42,7 @@ const MovieID = () => {
             </span>
             <span
               style={{
-                float: 'left',
+                float: "left",
                 color: "#e9ecef",
                 opacity: "0.75",
                 fontSize: "13px",
@@ -53,7 +53,7 @@ const MovieID = () => {
             </span>
             <span
               style={{
-                float: 'left',
+                float: "left",
                 color: "#e9ecef",
                 opacity: "0.75",
                 fontSize: "13px",
@@ -76,80 +76,103 @@ const MovieID = () => {
             </Link>
           </div>
         </div>
-        <div className='con'>
-          <div
-            className='seatsStatus'
-            style={{
-              backgroundColor: "#182356",
-              display: "flex",
-              marginTop: "80px",
-              paddingLeft: "10px"
-            }}
-          >
-            <ul
+        <div className="con">
+          <div>
+            <div
+              className="seatsStatus"
               style={{
+                backgroundColor: "#182356",
                 display: "flex",
-                padding: "0",
-                listStyle: "none"
+                marginTop: "80px",
+                paddingLeft: "10px",
               }}
             >
-              <li style={{ marginRight: "0px" }}>
-                <span
-                  style={{
-                    fontSize: "10px",
-                    background: "#323645",
-                    display: "inline-block"
-                  }}
-                ></span>
-                AVAILABLE
-              </li>
-              <li style={{ marginRight: "0px" }}>
-                <span
-                  style={{
-                    fontSize: "10px",
-                    background: "#081548",
-                    display: "inline-block"
-                  }}
-                ></span>
-                SOCIAL DISTANCING
-              </li>
-              <li style={{ marginRight: "0px" }}>
-                <span
-                  style={{
-                    fontSize: "10px",
-                    background: "#f2f2f2",
-                    display: "inline-block"
-                  }}
-                ></span>
-                SELECTED
-              </li>
-              <li style={{ marginRight: "0px" }}>
-                <span
-                  style={{
-                    fontSize: "10px",
-                    background: "#ffc107",
-                    display: "inline-block"
-                  }}
-                ></span>
-                BOOKED
-              </li>
-              <li style={{ marginRight: "15px" }}>
-                <span
-                  style={{
-                    fontSize: "10px",
-                    background: "#e11e26",
-                    display: "inline-block",
-                    cursor: "not-allowed",
-                  }}
-                ></span>
-                SOLD
-              </li>
-            </ul>
+              <ul
+                style={{
+                  display: "flex",
+                  padding: "0",
+                  listStyle: "none",
+                }}
+              >
+                <li style={{ marginRight: "0px" }}>
+                  <span
+                    style={{
+                      fontSize: "10px",
+                      background: "#323645",
+                      display: "inline-block",
+                    }}
+                  ></span>
+                  AVAILABLE
+                </li>
+                <li style={{ marginRight: "0px" }}>
+                  <span
+                    style={{
+                      fontSize: "10px",
+                      background: "#081548",
+                      display: "inline-block",
+                    }}
+                  ></span>
+                  SOCIAL DISTANCING
+                </li>
+                <li style={{ marginRight: "0px" }}>
+                  <span
+                    style={{
+                      fontSize: "10px",
+                      background: "#f2f2f2",
+                      display: "inline-block",
+                    }}
+                  ></span>
+                  SELECTED
+                </li>
+                <li style={{ marginRight: "0px" }}>
+                  <span
+                    style={{
+                      fontSize: "10px",
+                      background: "#ffc107",
+                      display: "inline-block",
+                    }}
+                  ></span>
+                  BOOKED
+                </li>
+                <li style={{ marginRight: "15px" }}>
+                  <span
+                    style={{
+                      fontSize: "10px",
+                      background: "#e11e26",
+                      display: "inline-block",
+                      cursor: "not-allowed",
+                    }}
+                  ></span>
+                  SOLD
+                </li>
+              </ul>
+            </div>
+            <div className="all_seats">
+
+            </div>
+            <div className="screen">Screen</div>
           </div>
+          <div className="total_process">
+            <div className="inner">
+              <div className="amount">
+                <div className="inner_amount">
+                  <h2>Total: &nbsp;</h2>
+                  <h2>Rs.&nbsp;</h2>
+                  <h2>0.00</h2>
+                </div>
+              </div>
+              <div className="proceed">
+                <div className="inner_proceed">
+                  <span className="proceeder floater">Proceed</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </>
   );
-}
+};
 
 export default MovieID;
