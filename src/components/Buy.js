4e721abd15fec4f58f572f63a3cf8428d9576da4
Swoyesh Shrestha <swoyeshshrestha69@gmail.com>
2024-06-07 22:03:13 +0545

    all things getting added

---
+++ src/components/Buy.js
@@ -0,0 +1,246 @@
+import React, { useState, useContext } from 'react'
+import { Link } from 'react-router-dom'
+import Layout from "./Layout";
+import "../Buy.css";
+import movieContext from '../Context/movieContext'
+
+const Buy = () => {
+  const context = useContext(movieContext)
+  const audis = ["Adui 1", "Audi 2", "Audi 3"]
+  const time = ['10:00 AM', "14:00 PM"]
+  const { setDay } = context
+  const d = new Date();
+  const ld = new Date(d.getFullYear(), d.getMonth()+1, 0)
+  const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+  let arr = [];
+  let newArr = [];
+  for (let index = 0; index < 8; index++) {
+    if(index === 0){
+      if(d.getDate() + 2>ld.getDate()){
+        arr[index] = 1;
+      }else{
+        arr[index] = d.getDate() + 2
+      }
+    }else{
+      if(arr[index-1] === ld.getDate()){
+        arr[index] = 1
+      }else{
+        arr[index] = arr[index-1] + 1;
+      }    
+    } 
+  }
+
+  const cm = (index)=>{
+    if(arr[index]<=ld.getDate() && arr[index]>=ld.getDate()-8){
+      return d.getMonth()
+    }else{
+      return d. getMonth()+1
+    }
+  }
+
+  for (let index = 0; index < 8; index++) {
+    newArr[index] = arr[index].toString();
+  }
+  let [activeTab, setActiveTab] = useState("Today")
+    let style = {
+        color: "white",
+        fontSize: "30px"
+    }
+    let clicked11 = (state)=>{
+      setActiveTab(state)
+      setDay("today")
+    }
+    let clicked12 = (state)=>{
+      setActiveTab(state)
+      setDay("tomm")
+    }
+    let clicked2 = (state)=>{
+      setActiveTab(state)
+    }
+  return (
+    <Layout>
+      <div className="ancs">
+        <div
+          className="container"
+          style={{ height: "750px", display: "flex", flexDirection: "column" }}
+        >
+          <div style={{ width: "100%" }}>
+            <div
+              className="firsts"
+              style={{ width: "100%", position: "relative" }}
+            >
+              <img
+                src="https://variety.com/wp-content/uploads/2023/08/ONEPIECE_Unit_10613RC.jpg"
+                alt="One Piece"
+                style={{
+                  width: "100%",
+                  height: "450px",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  zIndex: 0,
+                }}
+              />
+              <div
+                className="lasts"
+                style={{ width: "100%", position: "relative", top: "350px" }}
+              ></div>
+            </div>
+          </div>
+          <div
+            style={{
+              position: "relative",
+              margin: "10px",
+              width: "100%",
+              top: "-95px",
+            }}
+          >
+            <div style={{ position: "relative", zIndex: "3" }}>
+              <img
+                src="https://dnm.nflximg.net/api/v6/2DuQlx0fM4wd1nzqm5BFBi6ILa8/AAAAQfOO_2CUNrBbD6ZqpzaczOc69ZiUc9lmGb-HDMYOCCamtFesr2mt66xYkOa7gj7rML3zHArPoJiRdm1uUX6qGQF6wDhw56bjCzWRrAPf4rOtoVXKzcTXNuph6N5IuG4iP035pbohFGrTQyhtKXDEL30d.jpg?r=409"
+                height="340px"
+                width="275px"
+                style={{ borderRadius: "10px" }}
+              ></img>
+              <div
+                className="info"
+                style={{
+                  color: "white",
+                  display: "flex",
+                  left: "320px",
+                  position: "absolute",
+                  flexDirection: "column",
+                  top: "10px",
+                }}
+              >
+                <h1
+                  style={{
+                    fontWeight: "600",
+                    fontSize: "36px",
+                    lineHeight: "1.2",
+                  }}
+                >
+                  One Piece
+                </h1>
+                <div style={{ display: "flex", position: "relative", top: "-10px"}}>
+                  <hr
+                    width="25vw"
+                    style={{ border: "1.35px solid red", opacity: "1" }}
+                  />
+                  <h4
+                    style={{
+                      fontWeight: "400",
+                      fontSize: "13px",
+                      margin: "7px",
+                      lineHeight: "1.5",
+                    }}
+                  >
+                    Action, Adventure
+                  </h4>
+                </div>
+                <p
+                  className="movie-info 2my"
+                  style={{
+                    fontSize: "15px",
+                    fontWeight: "300",
+                    lineHeight: "3", /* Adjusted line height */
+                  }}
+                >
+                  The greatest story of all time!!
+                </p>
+                <div className="justified-list" style={{ lineHeight: "1.5" , display: "flex", width: "100%", flexDirection: "row"}}>
+                  <div style={{position: 'absolute', flex: "1"}}>
+                  <div className="item">
+                    <span style={{ color: "#db322b" }}>Director : &nbsp;</span>{" "}
+                    {/* Eiichiro Oda */}
+                  </div>
+                  <div className="item">
+                    <span style={{ color: "#db322b" }}>Cast : &nbsp;</span>{" "}
+                    {/* Luffy, Zoro, Nami */}
+                  </div>
+                  <div className="item">
+                    <span style={{ color: "#db322b" }}>
+                      Release On : &nbsp;
+                    </span>{" "}
+                    {/* June 5, 2024 */}
+                  </div>
+                  <div className="item">
+                    <span style={{ color: "#db322b" }}>Director : &nbsp;</span>{" "}
+                    {/* Infinity */}
+                  </div>
+                  </div>
+                  <div style={{ position: "absolute", width: "100%", left: "120px"}}>
+                  <div className="item">
+                    Eiichiro Oda
+                  </div>
+                  <div className="item">
+                    Luffy, Zoro, Nami
+                  </div>
+                  <div className="item">
+                    June 5, 2024
+                  </div>
+                  <div className="item">
+                    Infinity
+                  </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container" style={{ flexDirection: "column", width: "88%"}}>
+        <h3 style={{ fontWeight: "bold", color: "white" }}>
+          Viewing Times{" "}
+          <span
+            style={{
+              backgroundColor: "red",
+              height: "5px",
+              width: "5px",
+              display: "inline-block",
+              borderRadius: "50%",
+            }}
+          ></span>
+        </h3>
+        <div style={{alignSelf: "flex-end", height: "0px", alignContent: "center", position: "absolute"}}>
+      <Link className={activeTab==="Today"?'actors':'dices'} onClick={()=>clicked11("Today")}>Today</Link>
+          <Link className={activeTab==="Tomm"?'actors':'dices'} onClick={()=>clicked12("Tomm")}>Tomm</Link>
+          <Link className={activeTab==="1"?'actors':'dices'} onClick={()=>clicked2("1")}>{newArr[0].concat(" ", month[cm(0)])}</Link>
+          <Link className={activeTab==="2"?'actors':'dices'} onClick={()=>clicked2("2")}>{newArr[1].concat(" ", month[cm(1)])}</Link>
+          <Link className={activeTab==="3"?'actors':'dices'} onClick={()=>clicked2("3")}>{newArr[2].concat(" ", month[cm(2)])}</Link>
+          </div>
+        <div style={{ display: "flex" }}>
+          <div>
+            <span className="redr">o</span>
+            <span className="niceee">SOLD</span>
+          </div>
+          <div>
+            <span className="yellowy">o</span>
+            <span className="nicee1">BOOKED</span>
+          </div>
+          <div>
+            <span className="greeng">o</span>
+            <span className="nicee2">AVAILABLE</span>
+          </div>
+        </div>
+      </div>
+      <div className='ances' style={{margin: "20px", marginBottom: "60px"}}>
+        
+          {audis.map((element)=>{
+            return <div className='container' style={{height: "110px", borderRadius: "10px", width: "90%", marginBottom: "20px", display: "flex", flexDirection: "row"}}>
+            <div style={{backgroundColor: "#db322b", width: "14%", display: "flex", alignItems: "flex-start", justifyContent: "flex-start", borderRadius: "10px 0px 0px 10px", position: "relative", left: "-12px"}}>
+            <span style={{color: "white", fontWeight: "bold", margin: "5px", marginTop: "10px", marginLeft: "16px", display: "inline-block", fontSize: "22px", fontFamily: "Arial Black", position: "absolute"}}>{element}</span>
+            </div>
+            <div style={{display: "flex", alignItems: "flex-end"}}>
+              {time.map((elements)=>{
+                return <a className='timerr' style={{width: "82px"}}>{elements}</a>
+              })}
+          </div>
+        </div>
+          })}
+      </div>
+    </Layout>
+  );
+};
+
+export default Buy;
---
+++ src/components/Cmovie.js
@@ -0,0 +1,189 @@
+//individual movies
+
+import React, { useContext, useState} from "react";
+import "../cMovie.css";
+import { Link } from "react-router-dom";
+import movieContext from "../Context/movieContext";
+
+const Cmovie = (props) => {
+  const context = useContext(movieContext)
+  const {acitveTab, setActiveTab, day, setDay} = context
+  const [display1, setDisplay1] = useState(-140);
+  const [zoom, setZoom] = useState("1");
+  const { type } = props;
+  let hello = []
+  
+  props.time.forEach(element => {
+    hello.push(element.split(":"))
+  });
+
+  console.log(hello)
+  const st1 = {
+    fontSize: "12.5px",
+    width: "60px",
+  };
+  const date = new Date();
+  const hours = date.getHours();
+  const min = date.getMinutes();
+  console.log(date.getTime())
+  const st2 = {
+    fontSize: "12.5px",
+    width: "60px",
+    cursor: "not-allowed",
+    opacity: "0.6",
+    pointerEvents: "auto",
+    color: "gray",
+    border: "1px solid gray",
+    backgroundColor: "#182356",
+  };
+
+  const [choose, setChoose] = useState("1");
+  const clicked1 = () => {
+    setZoom("1.1");
+    setDisplay1(0);
+  };
+
+  const clicked2 = () => {
+    setZoom("1");
+    setDisplay1(-140);
+  };
+
+  let s1 = {
+    translate: display1,
+  };
+  let s2 = { 
+    scale: zoom,
+  };
+
+  const chooser = (e) => {
+    if (hours > parseInt(e)) {
+      setChoose("2");
+    }
+  };
+
+  const constant = () => {
+    setChoose("1");
+  };
+
+  const fs1 = {
+    textDecoration: "none", 
+    color: "red"
+  }
+  const fs2 = {
+    textDecoration: "none", 
+    color: "white"
+  }
+const [nice, setNice] = useState(fs1)
+
+const mover = ()=>{
+  setNice(fs2)
+}
+
+const leaverr = ()=>{
+  setNice(fs1)
+}
+
+const newer = ()=>{
+  setActiveTab("")
+}
+
+
+
+
+  return (
+    <>
+      <div
+        className="card"
+        style={{ width: "25%", margin: "15px", border: "none" }}
+      >
+        <div
+          onMouseMove={clicked1}
+          onMouseOut={clicked2}
+          className="type"
+          style={{
+            position: "relative",
+            top: "10px",
+            backgroundColor: "red",
+            height: "0%",
+            textAlign: "right",
+            right: "10px",
+          }}
+        > <Link to="/buy" style={nice}>
+          <button className="b1" style={s1} onMouseMove={mover} onMouseLeave={leaverr} onClick={newer}>
+           <i class="fa-regular fa-ticket" style={{ margin: "8px"}}></i>Buy
+            Tickets
+          </button>
+          </Link>
+          <button className="b2" style={s1}>
+            <i class="fa-thin fa-play" style={{ margin: "8px" }}></i>Play
+            Trailer
+          </button>
+          <button className="buttt">{type}</button>
+        </div>
+        <div className="main" style={{ height: "380px" }}>
+          <div
+            className="img-div"
+            style={s2}
+            onMouseMove={clicked1}
+            onMouseOut={clicked2}
+          >
+            <img
+              src={props.img}
+              className="card-img-top"
+              alt="..."
+              height={"380px"}
+            />
+          </div>
+        </div>
+        <div
+          className="card-body"
+          style={{ backgroundColor: "#182356", width: "100%" }}
+        >
+          <h5
+            className="card-title"
+            style={{ color: "white", fontSize: "21px" }}
+          >
+            <strong>{props.title}</strong>
+          </h5>
+          <p
+            className="card-text"
+            style={{ color: "gray", fontSize: "12px", textAlign: "left" }}
+          >
+            {props.duration}
+          </p>
+          <p
+            className="card-text"
+            style={{
+              color: "gray",
+              marginTop: "-12px",
+              fontSize: "12px",
+              textAlign: "left",
+            }}
+          >
+            {props.genre}
+          </p>
+          {hello.map((element) => {
+            const authToken = localStorage.getItem("auth-token");
+            return (
+              <Link
+                to={authToken === null && choose == "1" ? "/login" : "/movieid"}
+                key={element}
+              >
+                <button
+                  className="btn btn-primary"
+                  style={hours < parseInt(element[0]) || day === "tomm" ? st1 : (hours == parseInt(element[0]) && min <= parseInt(element[1]) ? st1 : st2)}
+                  onMouseMove={() => chooser(element)}
+                  onMouseLeave={constant}
+                >
+                  {element[0]}:{element[1]}
+                </button>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Cmovie;
